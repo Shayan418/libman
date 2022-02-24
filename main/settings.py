149164b7c8e4b28b59183e42ce212c389dd51d01
jahanzebshayan <jahanzebshayan14@gmail.com>
2022-02-24 20:42:17 +0530

    os imported

--- main/settings.py
+++ main/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q*%###pv&nj2@ub_d!6@7ads7joku82u)*5ret@%ev4h$$1uz+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '[::1]', 'https://obscure-basin-15555.herokuapp.com/']
 
 
 # Application definition
